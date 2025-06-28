e3b06485349ee71d29f840102547912e783eb228
Iain Visa <59170929+Thecnopapa@users.noreply.github.com>
2025-06-28 20:08:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
+@app.route("/")
+def github():
+    return redirect("https://www.linkedin.com/in/iainvisa")
+
+@app.route("/linkedin")
+def github():
+    return redirect("https://www.linkedin.com/in/iainvisa")
+
+
 @app.route("/github")
 def github():
     return redirect("https://github.com/Thecnopapa")
