50ba66528250a0b3e8d4a5e9cce5f74b07d9728b
Iain Visa <59170929+Thecnopapa@users.noreply.github.com>
2025-06-28 19:41:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def github():
+    return redirect("https://github.com/Thecnopapa")
+
+@app.route("/ProjectDimer")
+def projectDimer():
     return redirect("https://github.com/Thecnopapa/projectDimer")
