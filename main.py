93ddd120dc27c1fc9387ecb098043f5af70b5a42
Iain Visa <59170929+Thecnopapa@users.noreply.github.com>
2025-06-28 12:09:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return (redirect("https://github.com/Thecnopapa/projectDimer")
+    return redirect("https://github.com/Thecnopapa/projectDimer")
