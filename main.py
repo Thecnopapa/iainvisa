591aedcbaf94d0b5f44cd41aabeb3101fa6c5e75
Iain Visa <59170929+Thecnopapa@users.noreply.github.com>
2025-07-08 11:44:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,18 @@ def linkedin():
 @app.route("/github")
 def github():
     return redirect("https://github.com/Thecnopapa")
-
+    
+@app.route("/projectdimer")    
+@app.route("/projectDimer")
 @app.route("/ProjectDimer")
 def projectDimer():
     return redirect("https://github.com/Thecnopapa/projectDimer")
+
+@app.route("/projectdimer/ppt")    
+@app.route("/projectDimer/ppt")
+@app.route("/ProjectDimer/PPT")
+def projectDimerPPT():
+    return redirect("https://docs.google.com/presentation/d/1HGT1u9emPj-3RmbbPaktof1PfAYAaBjy0TO_0LxUf3E/edit?usp=sharing")
+
+
+
